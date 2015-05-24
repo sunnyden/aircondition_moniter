167560f820c9c55f73d964066c0e198f82c43877
邓昊晴 <exeded@126.com>
2015-05-24 19:53:01 +0800

    add mq135 support

--- pm2d5_new.ino
+++ pm2d5_new.ino
@@ -1,7 +1,10 @@
+#include <dhq.h>
+#include <text.h>
+//above no use any more
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(4,5);
 int dustPin=0;
-
+int mq135Pin=1;
 float dustVal=0;
  
 int ledPower=2;
@@ -13,6 +16,7 @@ Serial.begin(9600);
 mySerial.begin(9600);
 pinMode(ledPower,OUTPUT);
 pinMode(dustPin, INPUT);
+pinMode(mq135Pin,INPUT);
 }
  
 void loop(){
@@ -23,9 +27,17 @@ dustVal=analogRead(dustPin);
 delayMicroseconds(delayTime2);
 digitalWrite(ledPower,HIGH); 
 delayMicroseconds(offTime);
- 
+String pm2d5="{\"pm2d5\": \"null\"}";
 delay(1000);
-if (dustVal>36.455)
-Serial.println((float(dustVal/1024)-0.0356)*120000*0.035);
-mySerial.println((float(dustVal/1024)-0.0356)*120000*0.035);
+if(dustVal>36.455){
+  float Vo135=analogRead(mq135Pin);
+Serial.println((float(dustVal/1024)-0.0356)*120000*0.035/4);
+Serial.println(0.6*1000*(Vo135/1024)*(5000/4000));
+
+mySerial.print("{\"pm2d5\":\"");
+mySerial.print((float(dustVal/1024)-0.0356)*120000*0.035/4);
+mySerial.print("\",\"mq135\":\"");
+mySerial.print(0.6*1000*(Vo135/1024)*(5000/4000));
+mySerial.println("\"}");
+}
 }
